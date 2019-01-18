73374f25b0cfdc81a07b1295c58406cfb3ceb5ec
atdavids2 <atdavids2@gmail.com>
2019-01-17 23:24:37 -0800

    Added navigation initial app pages

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import { Component } from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import { Home } from './components/Home';
+import { Requests } from './components/Requests';
+import { Calendar } from './components/Calendar';
+import { Contacts } from './components/Contacts';
+import { Alerts } from './components/Alerts';
+import { createBottomTabNavigator, createAppContainer, createStackNavigator } from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,34 +15,73 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+let TabNavigator = createBottomTabNavigator(
+  {
+    Home: Home,
+    Requests: Requests,
+    Calendar: Calendar,
+    Contacts: Contacts,
+    Alerts: Alerts
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `ios-home`;
+        }
+        else if (routeName === 'Requests') {
+          iconName = 'ios-add-circle';
+        }
+        else if (routeName === 'Calendar') {
+          iconName = 'ios-calendar';
+        }
+        else if (routeName === 'Contacts') {
+          iconName = 'ios-call';
+        }
+        else if (routeName === 'Alerts') {
+          iconName = 'ios-warning';
+        }
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: '#fff',
+      inactiveTintColor: 'gray',
+      showLabel: false,
+      style: {
+        backgroundColor: '#f4511e'
+      }
+    },
+  }
+);
+
+let AppNavigator = createStackNavigator(
+  {
+    Tabs: TabNavigator
+  },
+  {
+    defaultNavigationOptions: {
+      title: 'Chicago',
+      headerStyle: {
+        backgroundColor: '#f4511e'
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        flex: 1
+      },
+    }
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Schaab sucks!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+    return <AppContainer />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
